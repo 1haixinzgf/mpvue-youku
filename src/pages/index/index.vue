ef6a7ed587ffda4b0ce203d1e4fc0d206f4b0f49
unknown <2725965600@qq.com>
2018-07-08 23:58:05 +0800

    增加api和common文件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,68 +1,73 @@
 <template>
-<div class="container">
-  <div class="header">
-    <div class="header-title">
-      <div class="header-title-left">
-        <img src="../../../static/images/yk-search.png" alt="" class="header-title-pic">
+<div id="app">
+  <div class="container">
+    <!-- <div class="header">
+      <div class="header-title">
+        <div class="header-title-left">
+          <img src="../../../static/images/yk-search.png" alt="" class="header-title-pic">
+        </div>
+        <div class="header-title-right">
+        <scrollview></scrollview>
+        </div>
       </div>
-      <div class="header-title-right">
-      <scrollview></scrollview>
+      <div class="header-content">
+        <yk-input />
+        <image-box></image-box>
+        <image-box></image-box>
+        <div style="margin-right: 10rpx">
+        <image-box ></image-box></div>
       </div>
-    </div>
-    <div class="header-content">
-      <yk-input />
-      <image-box></image-box>
-      <image-box></image-box>
-      <div style="margin-right: 10rpx">
-      <image-box ></image-box></div>
+    </div> -->
+    <div class="swiper-content">
+      <v-swiper></v-swiper>
     </div>
   </div>
-  <!-- <div class="swiper-content">
-    <swiper></swiper>
-  </div> -->
 </div>
-  
 </template>
 
 <script>
 import scrollview from '@/components/scrollview/scrollview'
 import input from '@/components/input/input'
 import imageBox from '@/components/imageBox/imageBox'
-import swiper from '@/components/swiper/swiper'
+import swipe from '@/components/swiper/swipe'
 export default {
   components: {
     scrollview,
     'yk-input': input,
     imageBox,
-    swiper
+    'v-swiper':swipe
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.header
-  background-color #131775
-  height 155rpx
+.container
   width 100%
-  .header-title
-    height 80rpx
-    display flex
-    justify-content space-between
-    .header-title-left
-      width 60rpx
-      height 60rpx
-      margin-left 10rpx
-      margin-top 10rpx
-      .header-title-pic
-        width 100%
-        height 100%
-    .header-title-right
-      width 620rpx
-      height 80rpx
-      margin-right 20rpx
+  height 200rpx
+  // background-color red
+// .header
+//   background-color #131775
+//   height 155rpx
+//   width 100%
+//   .header-title
+//     height 80rpx
+//     display flex
+//     justify-content space-between
+//     .header-title-left
+//       width 60rpx
+//       height 60rpx
+//       margin-left 10rpx
+//       margin-top 10rpx
+//       .header-title-pic
+//         width 100%
+//         height 100%
+//     .header-title-right
+//       width 620rpx
+//       height 80rpx
+//       margin-right 20rpx
 
-  .header-content 
-    display flex
-    justify-content space-around
-    text-align center
+//   .header-content 
+//     display flex
+//     justify-content space-around
+//     text-align center
 </style>
