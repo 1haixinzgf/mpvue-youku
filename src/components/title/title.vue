f17c4a7a09d1a9fe56bcf6905fbd1b9960796b34
unknown <2725965600@qq.com>
2018-07-22 23:57:16 +0800

    引入weui，封装weui成理想组件

---
+++ src/components/title/title.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="title">
+        <slot name="img" v-if="isShowImg"/>
+        <div class="title-content">{{title}}</div>
+        <slot name="navigato"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        isShowImg: Boolean
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import  '../../common/style/mixin'
+    .title
+        wh(100%, 80rpx)
+        .img
+</style>
+
---
+++ src/components/title/weuiNo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div  class="weui-cell weui-cell_access" hover-class="weui-cell_active">
+        <div class="weui-cell__bd font-text">{{titleText}}}</div>
+        <div v-if="isShowTo" class="weui-cell__ft weui-cell__ft_in-access font-to" ></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titleText: Boolean,
+        isShowTo: {
+            type: Boolean,
+            default: true
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+    .font-text
+        font-weight bold
+        font-size 30rpx
+    .font-to
+        font-size 23rpx
+</style>
+
